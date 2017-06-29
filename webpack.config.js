0cbe9b8b1bdb5f48d778ff4d59823f2c5a4394f1
douglasvegas <964273641@qq.com>
2017-06-29 11:51:34 +0800

    use yarn and add code check

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,51 @@
-var path = require('path')
-var webpack = require('webpack')
+const path = require('path')
+const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const argv = require('yargs').argv;
-const modelName = argv.env.M;
-const contextUrl = path.resolve(__dirname, modelName);
+const argv = require('yargs').argv
+
+const modelName = argv.env.M
+const contextUrl = path.resolve(__dirname, modelName)
 module.exports = {
-    devtool: 'source-map',
-    context: contextUrl,
-    entry: './src/app',
-    output: {
-        filename: 'js/bundle.js',
-        path: path.resolve(contextUrl, '/build'),
-        publicPath:'http://localhost:7000/',
-    },
-    module: {
-        rules: [
-        { 
-            test: /\.(js|jsx)$/, 
-            use: 'babel-loader', 
-            exclude: /node_modules/ 
-        },
-        {
-            test: /\.(css|scss)$/,
-            use: ['style-loader','css-loader','sass-loader']
-        }
-        ],
-    },
-    resolve: {
-        extensions: ['.js', '.jsx'],
-    },
-    devServer: {
-        port: 7000,
-        hot: true, // 4
-        // 开启服务器的模块热替换(HMR)
-        headers: {
-        'Access-Control-Allow-Origin': '*', // 5
-        },
-    },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin(), // 6
-        // 开启全局的模块热替换(HMR)
-        new webpack.NamedModulesPlugin(), // +
-        // 当模块热替换(HMR)时在浏览器控制台输出对用户更友好的模块名字信息
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            template: 'index.html',
-            inject: true
-        }),
+  devtool: 'source-map',
+  context: contextUrl,
+  entry: './src/app',
+  output: {
+    filename: 'js/bundle.js',
+    path: path.resolve(contextUrl, '/build'),
+    publicPath: 'http://localhost:7000/'
+  },
+  module: {
+    rules: [
+      { test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      { test: /\.(css|scss)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }
     ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    modules: [path.resolve(__dirname, 'node_modules')]
+  },
+  devServer: {
+    port: 7000,
+    hot: true, // 4
+    // 开启服务器的模块热替换(HMR)
+    headers: {
+      'Access-Control-Allow-Origin': '*' // 5
+    }
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(), // 6
+    // 开启全局的模块热替换(HMR)
+    new webpack.NamedModulesPlugin(), // +
+    // 当模块热替换(HMR)时在浏览器控制台输出对用户更友好的模块名字信息
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'index.html',
+      inject: true
+    })
+  ]
 }
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,96 +1,95 @@
 const path = require('path')
-const fs   = require('fs')
 const webpack = require('webpack')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const WebpackMd5Hash = require('webpack-md5-hash')
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+
 const nodemodulesPath = path.resolve(__dirname, 'node_modules')
 const pathToReact = path.resolve(nodemodulesPath, 'react/dist/react.min.js')
 const pathToReactDOM = path.resolve(nodemodulesPath, 'react-dom/dist/react-dom.min.js')
+const argv = require('yargs').argv
 
-const argv = require('yargs').argv;
-const modelName = argv.env.M;
-const contextUrl = path.resolve(__dirname, modelName);
-const publicPath = './' + modelName + '/dist/';
-let buildFileName = 'index.html';
-if (modelName == 'main') {
-    buildFileName = path.resolve(__dirname) + '/index.html'
+const modelName = argv.env.M
+const contextUrl = path.resolve(__dirname, modelName)
+const publicPath = `./${modelName}/dist/`
+let buildFileName = 'index.html'
+if (modelName === 'main') {
+  buildFileName = `${path.resolve(__dirname)}/index.html`
 }
-const buildPath = path.resolve(contextUrl, 'dist');
+const buildPath = path.resolve(contextUrl, 'dist')
 
 module.exports = {
-    context: contextUrl,
-    devtool: 'inline-source-map',
-    entry:{
-        index: ['./src/app'],
-        vendor: ['react', 'react-dom']
-    },
-    output: {
-        filename: '[name].[chunkhash:8].js',
-        path: buildPath,
-        publicPath: publicPath,
-    },
-    resolve:{
-        extensions: ['.js','.jsx','.scss','.css'],
-        modules: [
-            path.resolve(__dirname, 'node_modules'),
-            path.join(__dirname,'./src')
-        ],
-        alias: {
-            'react.js': pathToReact,
-            'react-dom.js': pathToReactDOM 
-        }
-    },
-    module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                use: ['babel-loader'],
-                exclude: path.resolve(__dirname,'node_modules')
-            },
-            {
-                test: /\.(css|scss)$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: ['css-loader','sass-loader']
-                })
-            }
-        ]
-    },
-    plugins: [
-        new webpack.optimize.OccurrenceOrderPlugin(),
-        new webpack.DefinePlugin({
-            'process.env': {
-                'NODE_ENV': "'production'"
-            }
-        }),
-        new ExtractTextPlugin('index.[contenthash:8].css'),
-        new WebpackMd5Hash(),
-        new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                warnings: false
-            }
-        }),
-        new HtmlWebpackPlugin({
-            filename: buildFileName,
-            template: 'index.html',
-            inject: true,
-            //chunks:['bundle'], //加载指定模块、否则加载所有模块
-            hash: false,
-            minify: {
-                removeComments: true,
-                collapseWhitespace: true
-            }
-        }),
-        new CleanWebpackPlugin(
-            [contextUrl + '/dist/*'], //匹配文件
-            {
-                root: __dirname, //根目录
-                verbose: true, //开启在控制台输出信息
-                dry: false //启用删除文件
-            }
-        )
-
+  context: contextUrl,
+  devtool: 'inline-source-map',
+  entry: {
+    index: ['./src/app'],
+    vendor: ['react', 'react-dom']
+  },
+  output: {
+    filename: '[name].[chunkhash:8].js',
+    path: buildPath,
+    publicPath
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.scss', '.css'],
+    modules: [
+      path.resolve(__dirname, 'node_modules'),
+      path.join(__dirname, './src')
     ],
+    alias: {
+      'react.js': pathToReact,
+      'react-dom.js': pathToReactDOM
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: ['babel-loader'],
+        exclude: path.resolve(__dirname, 'node_modules')
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: "'production'"
+      }
+    }),
+    new ExtractTextPlugin('index.[contenthash:8].css'),
+    new WebpackMd5Hash(),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    }),
+    new HtmlWebpackPlugin({
+      filename: buildFileName,
+      template: 'index.html',
+      inject: true,
+      // chunks:['bundle'], //加载指定模块、否则加载所有模块
+      hash: false,
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true
+      }
+    }),
+    new CleanWebpackPlugin(
+      [`${contextUrl}/dist/*`], // 匹配文件
+      {
+        root: __dirname, // 根目录
+        verbose: true, // 开启在控制台输出信息
+        dry: false  // 启用删除文件
+      }
+        )
+  ]
 }
